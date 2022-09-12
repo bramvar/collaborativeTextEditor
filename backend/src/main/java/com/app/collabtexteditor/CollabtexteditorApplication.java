df98aa20a616e4b9107053d5665d07fc72379fe6
vardo159 <bramf0@protonmail.com>
2022-09-11 20:42:55 -0500

    initialize backend module

---
+++ backend/src/main/java/com/app/collabtexteditor/CollabtexteditorApplication.java
@@ -0,0 +1,13 @@
+package com.app.collabtexteditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabtexteditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabtexteditorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/collabtexteditor/CollabtexteditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.collabtexteditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabtexteditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
